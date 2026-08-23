c6525e11fc093c78c882cd43d22567d0dca14dd0
NusanHuh <adityaparama119@gmail.com>
2026-08-23 20:57:07 +0700

    Add icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/icon.png';
 	import Lenis from 'lenis';
 	import { onMount } from 'svelte';
 	import { lenisStore } from '$lib/lenis.svelte';
